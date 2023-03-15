1b9b1fc83db33d8bfee12e3f373dcdaebd080f0b
Abraxas-365 <luisfmiranda8@gmail.com>
2023-03-15 18:58:56 -0500

    actualizando

--- services/nft-creator/src/nft/infrastructure/contract/contract.ts
+++ services/nft-creator/src/nft/infrastructure/contract/contract.ts
@@ -16,8 +16,7 @@ export class Contract implements IContract {
     'https://polygon-mumbai.g.alchemy.com/v2/eF5ZSdI_QN11xi7VnLxT3LwpOYI4bM2B',
   ); //alchemy api
   protected publicKey = '0xA74Fd97Ed4561eA7e37EB0B22005f2Bd57Ec6b56';
-  protected privateKey =
-    'f12968a9fbf37694f2265d1360fa7f9f06fba26181d4a0340a35c8ad9497e6e5';
+  protected privateKey = '<privateKey>';
   protected nftContract = new this.web3.eth.Contract(
     this.contract.abi,
     this.contractAddress,
